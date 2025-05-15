f5cdc6cd41323102078f3a2131872817b1ea619f
MatebeleMokwena <142404003+MatebeleMokwena@users.noreply.github.com>
2025-05-15 11:00:31 +0200

    Add files via upload
    init_commit

---
+++ GotYouDataBase/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace GotYouDataBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMon = new Button();
+            btnExp = new Button();
+            btnClr = new Button();
+            btnSrch = new Button();
+            btnFil = new Button();
+            btnSave = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            chkCh = new CheckBox();
+            chkRe = new CheckBox();
+            chkCre = new CheckBox();
+            chkDel = new CheckBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnMon
+            // 
+            btnMon.Location = new Point(59, 85);
+            btnMon.Name = "btnMon";
+            btnMon.Size = new Size(94, 29);
+            btnMon.TabIndex = 0;
+            btnMon.Text = "...";
+            btnMon.UseVisualStyleBackColor = true;
+            btnMon.Click += btnMon_Click;
+            // 
+            // btnExp
+            // 
+            btnExp.Location = new Point(368, 85);
+            btnExp.Name = "btnExp";
+            btnExp.Size = new Size(94, 29);
+            btnExp.TabIndex = 1;
+            btnExp.Text = "Export log";
+            btnExp.UseVisualStyleBackColor = true;
+            btnExp.Click += btnExp_Click;
+            // 
+            // btnClr
+            // 
+            btnClr.Location = new Point(667, 85);
+            btnClr.Name = "btnClr";
+            btnClr.Size = new Size(94, 29);
+            btnClr.TabIndex = 2;
+            btnClr.Text = "Clear";
+            btnClr.UseVisualStyleBackColor = true;
+            btnClr.Click += btnClr_Click;
+            // 
+            // btnSrch
+            // 
+            btnSrch.Location = new Point(967, 85);
+            btnSrch.Name = "btnSrch";
+            btnSrch.Size = new Size(94, 29);
+            btnSrch.TabIndex = 3;
+            btnSrch.Text = "Search";
+            btnSrch.UseVisualStyleBackColor = true;
+            // 
+            // btnFil
+            // 
+            btnFil.Location = new Point(287, 53);
+            btnFil.Name = "btnFil";
+            btnFil.Size = new Size(94, 29);
+            btnFil.TabIndex = 4;
+            btnFil.Text = "Filter";
+            btnFil.UseVisualStyleBackColor = true;
+            btnFil.Click += btnFil_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.None;
+            btnSave.Location = new Point(1207, 804);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click_1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableLayoutPanel1.SetColumnSpan(dataGridView1, 2);
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 163);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1666, 580);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(406, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Currently Monitoring:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Brown;
+            label2.Location = new Point(561, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(24, 23);
+            label2.TabIndex = 8;
+            label2.Text = "??";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(924, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(177, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.None;
+            tableLayoutPanel1.SetColumnSpan(groupBox1, 2);
+            groupBox1.Controls.Add(btnSrch);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(btnClr);
+            groupBox1.Controls.Add(btnMon);
+            groupBox1.Controls.Add(btnExp);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(266, 17);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1140, 125);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Anchor = AnchorStyles.None;
+            groupBox3.Controls.Add(chkCh);
+            groupBox3.Controls.Add(chkRe);
+            groupBox3.Controls.Add(chkCre);
+            groupBox3.Controls.Add(chkDel);
+            groupBox3.Controls.Add(btnFil);
+            groupBox3.Location = new Point(205, 751);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(426, 135);
+            groupBox3.TabIndex = 11;
+            groupBox3.TabStop = false;
+            // 
+            // chkCh
+            // 
+            chkCh.AutoSize = true;
+            chkCh.Location = new Point(162, 82);
+            chkCh.Name = "chkCh";
+            chkCh.Size = new Size(90, 24);
+            chkCh.TabIndex = 8;
+            chkCh.Text = "Changed";
+            chkCh.UseVisualStyleBackColor = true;
+            // 
+            // chkRe
+            // 
+            chkRe.AutoSize = true;
+            chkRe.Location = new Point(22, 82);
+            chkRe.Name = "chkRe";
+            chkRe.Size = new Size(94, 24);
+            chkRe.TabIndex = 7;
+            chkRe.Text = "Renamed";
+            chkRe.UseVisualStyleBackColor = true;
+            // 
+            // chkCre
+            // 
+            chkCre.AutoSize = true;
+            chkCre.Location = new Point(162, 36);
+            chkCre.Name = "chkCre";
+            chkCre.Size = new Size(83, 24);
+            chkCre.TabIndex = 6;
+            chkCre.Text = "Created";
+            chkCre.UseVisualStyleBackColor = true;
+            // 
+            // chkDel
+            // 
+            chkDel.AutoSize = true;
+            chkDel.Location = new Point(22, 37);
+            chkDel.Name = "chkDel";
+            chkDel.Size = new Size(84, 24);
+            chkDel.TabIndex = 5;
+            chkDel.Text = "Deleted";
+            chkDel.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(btnSave, 1, 2);
+            tableLayoutPanel1.Controls.Add(groupBox3, 0, 2);
+            tableLayoutPanel1.Controls.Add(dataGridView1, 0, 1);
+            tableLayoutPanel1.Controls.Add(groupBox1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 21.4477215F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 78.55228F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 144F));
+            tableLayoutPanel1.Size = new Size(1672, 891);
+            tableLayoutPanel1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1672, 891);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMon;
+        private Button btnExp;
+        private Button btnClr;
+        private Button btnSrch;
+        private Button btnFil;
+        private Button btnSave;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private CheckBox chkCh;
+        private CheckBox chkRe;
+        private CheckBox chkCre;
+        private CheckBox chkDel;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ GotYouDataBase/Form1.cs
@@ -0,0 +1,240 @@
+using System;
+using System.Data.SQLite;
+
+namespace GotYouDataBase
+{
+    public partial class Form1 : Form
+    {
+
+        private FileSystemWatcher? watcher;
+        private Dictionary<string, long>? fSize = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.Columns.Add("Path", "Path");
+            dataGridView1.Columns.Add("ChangeType", "ChangeType");
+            dataGridView1.Columns.Add("Time", "Time");
+            dataGridView1.Columns.Add("Size", "Size");
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)//////////////////////////////////////////// 
+        {
+            this.BeginInvoke((MethodInvoker)(() =>
+            {
+                if (e.FullPath.EndsWith(".ini", StringComparison.OrdinalIgnoreCase)) return;
+
+                string changeType = $"{e.Name} was {e.ChangeType}";
+                string timeStamp = DateTime.Now.ToString("g");
+                string sizeText = "";
+
+                // Handle Deleted
+                if (e.ChangeType == WatcherChangeTypes.Deleted)
+                {
+                    dataGridView1.Rows.Add(e.FullPath, changeType, timeStamp, "Deleted");
+                    fSize?.Remove(e.FullPath); // Remove tracked size
+                    return;
+                }
+
+                long fileSize;
+                try
+                {
+                    FileInfo fileInfo = new FileInfo(e.FullPath);
+                    if (!fileInfo.Exists)
+                    {
+                        dataGridView1.Rows.Add(e.FullPath, changeType, timeStamp, "Not found");
+                        return;
+                    }
+
+                    fileSize = fileInfo.Length;
+                }
+                catch
+                {
+                    dataGridView1.Rows.Add(e.FullPath, changeType, timeStamp, "Inaccessible");
+                    return;
+                }
+
+                if (fSize != null && fSize.TryGetValue(e.FullPath, out long oldSize))
+                {
+                    if (oldSize != fileSize)
+                    {
+                        sizeText = $"{(oldSize / 1024 + 1)} KB => {(fileSize / 1024 + 1)} KB";
+                    }
+                    else
+                    {
+                        sizeText = $"{fileSize / 1024 + 1} KB";
+                    }
+                }
+                else
+                {
+                    sizeText = $"{fileSize / 1024 + 1} KB";
+                }
+
+                fSize[e.FullPath] = fileSize;
+
+                dataGridView1.Rows.Add(e.FullPath, changeType, timeStamp, sizeText);
+                dataGridView1.AutoResizeColumns();
+            }));
+
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e) //////////////////////////////////////
+        {
+            this.BeginInvoke((MethodInvoker)(() =>
+            {
+                string change = $"Renamed {e.OldName} =>> {e.Name}";
+                string time = DateTime.Now.ToString("g");
+                dataGridView1.Rows.Add(e.FullPath, change, time, "");
+            }));
+        }
+        public void SaveToQlite() //////////////////////////////////////////
+        {
+            string dbPath = Path.Combine(Application.StartupPath, "Monitored_Changes.db");
+            using var connect = new SQLiteConnection($"Data Source={dbPath}");
+
+            connect.Open();
+
+            var createTable = connect.CreateCommand();
+            createTable.CommandText =
+                @"CREATE TABLE IF NOT EXISTS Changes
+        (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Path TEXT,
+            ChangeType TEXT,
+            Time TEXT,
+            FileSize TEXT
+        );";
+            createTable.ExecuteNonQuery();
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                var insert = connect.CreateCommand();
+                insert.CommandText =
+                    @"
+              INSERT INTO Changes(Path, ChangeType, Time, FileSize)
+              VALUES($path, $changeType, $time, $size);";
+
+                insert.Parameters.AddWithValue("$path", row.Cells[0].Value?.ToString());
+                insert.Parameters.AddWithValue("$changeType", row.Cells[1].Value?.ToString());
+                insert.Parameters.AddWithValue("$time", row.Cells[2].Value?.ToString());
+                insert.Parameters.AddWithValue("$size", row.Cells[3].Value?.ToString());
+
+                insert.ExecuteNonQuery();
+            }
+            MessageBox.Show("Saved to SQLite DB", "Database", MessageBoxButtons.OK);
+        }
+
+
+        private void btnMon_Click(object sender, EventArgs e)/////////////////////////////////
+        {
+            //String to hold the path being monitered
+            String path;
+
+            using (var folder = new FolderBrowserDialog())
+            {
+                folder.Description = "Select File to Monitor";
+                DialogResult result = folder.ShowDialog();// dispaly explore folder
+
+                //If user clicks cancel or doesn't click ok 
+                if (result != DialogResult.OK || string.IsNullOrWhiteSpace(folder.SelectedPath)) return;
+
+                path = folder.SelectedPath;
+
+            }
+            label2.Text = Path.GetFileName(path) + ". . .";
+
+            //create a new instance of a watchere that also tracks subdirectories
+            watcher = new FileSystemWatcher(path);
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Size;
+
+            //Read any type of file
+            watcher.Filter = "*.*";
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnChanged;
+            watcher.Deleted += OnChanged;
+            watcher.Renamed += OnRenamed;
+
+        }
+
+        private void btnExp_Click(object sender, EventArgs e)/////////////////////////////////////////////
+        {
+            using (SaveFileDialog export = new())
+            {
+                export.Filter = "CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt;";
+
+                if (export.ShowDialog() != DialogResult.OK) return;
+
+                using StreamWriter write = new(export.FileName);
+                write.WriteLine("Path,Change Type,Time,Size");
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    var cells = row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString()?.Replace(",", " ") ?? "");
+                    string line = string.Join(",", cells);
+                    write.WriteLine(line);
+                }
+
+                MessageBox.Show("Export complete", "Done", MessageBoxButtons.OK);
+            }
+        }
+
+
+
+        private void btnSave_Click_1(object sender, EventArgs e)//////////////////////////////////////////
+        {
+            SaveToQlite();
+        }
+
+        private void btnFil_Click(object sender, EventArgs e)/////////////////////////////////////////
+        {
+            dataGridView1.Rows.Clear();
+
+            var conditions = new List<string>();
+
+            if (chkCre.Checked)
+                conditions.Add("ChangeType LIKE '%Created%'");
+            if (chkDel.Checked)
+                conditions.Add("ChangeType LIKE '%Deleted%'");
+            if (chkRe.Checked)
+                conditions.Add("ChangeType LIKE '%Renamed%'");
+            if (chkCh.Checked)
+                conditions.Add("ChangeType LIKE '%Changed%'");
+
+            string whereClause = conditions.Count > 0 ? "WHERE " + string.Join(" OR ", conditions) : "";
+
+            string dbPath = Path.Combine(Application.StartupPath, "Monitored_Changes.db");
+            using var connection = new SQLiteConnection($"Data Source={dbPath}");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = $"SELECT Path, ChangeType, Time, FileSize FROM Changes {whereClause}";
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                dataGridView1.Rows.Add(
+                    reader["Path"].ToString(),
+                    reader["ChangeType"].ToString(),
+                    reader["Time"].ToString(),
+                    reader["FileSize"].ToString()
+                );
+            }
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+    }
+}
